880b1063b58d6c3e5d12225e2ba6454dcb268967
adaam2 <adamjamesbull@googlemail.com>
2015-04-24 14:47:03 +0100

    added map element

--- TwitterClient.Web/Scripts/application.js
+++ TwitterClient.Web/Scripts/application.js
@@ -1,13 +1,34 @@
-﻿// document ready shorthand
+﻿var map;
+function initialize() {
+    var mapOptions = {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+}
+// document ready shorthand
+google.maps.event.addDomListener(window, 'load', initialize);
+
 $(function () {
     // obtain reference to the hub proxy and hub itself
     var theHub = $.connection.twitterHub;
 
-
     // this is the function that the server will call to broadcast new tweets
     theHub.client.broadcast = function (tweet) {
-        var item = '<li>' + tweet.text + '</li>';
-        $('ul.tweets').prepend(item);
+
+        var c = tweet.coordinates.coordinates;
+
+        if (!$.isEmptyObject(tweet.coordinates)) {
+            //console.log(c);
+            // To add the marker to the map, use the 'map' property
+            var marker = new google.maps.Marker({
+                position: { lat: c[1], lng: c[0] },
+                map: map,
+                title: "Hello World!"
+            });
+        }
+        
     };
 
     // this is a function that indicates that connection to the hub has been successful
